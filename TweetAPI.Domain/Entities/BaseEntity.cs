daae117df7b094ab46e9633df5192dae978ef2c1
Maor Eini <maor.eini@gmail.com>
2017-07-14 05:44:14 +0300

    Add Tweet model and BaseEntity

---
+++ TweetAPI.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TweetAPI.Domain.Entities
+{
+    public class BaseEntity
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement]
+        public DateTime CreatedOn { get; set; }
+
+        [BsonElement]
+        public DateTime LastModifiedOn { get; set; }
+    }
+}
---
+++ TweetAPI.Domain/Entities/Tweet.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TweetAPI.Domain.Entities
+{
+    public class Tweet : BaseEntity
+    {
+        [BsonElement("Content")]
+        public string Content { get; set; }
+
+        [BsonElement("Position")]
+        public GeoJsonPoint<GeoJson2DGeographicCoordinates> Position { get; set; }
+
+    }
+}
